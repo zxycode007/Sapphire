7ea9981bac2cf58173000e913ef43cc9963bc183
zxycode007 <373982141@qq.com>
2015-11-12 19:12:45 +0800

    3

--- SapphireBase/SapphireEdgeListBuilder.h
+++ SapphireBase/SapphireEdgeListBuilder.h
@@ -2,6 +2,7 @@
 #define _SAPPHIRE_EDGE_LIST_BUILDER_
 #include "SapphirePrerequisites.h"
 #include "SapphireLog.h"
+#include "SapphireVector3.h"
 
 namespace Sapphire
 {
@@ -45,8 +46,8 @@ namespace Sapphire
 			bool degenerate;
 		};
 
-		// 三角形面法线的4d向量数组，它是与这个三角形正交的单位向量， Array of 4D vector of triangle face normal, which is unit vector orthogonal
-		// to the triangles, 加上到原点的距离(4d)
+		// 三角形面法线的4d向量数组，它是与这个三角形正交的单位向量
+		// 加上到原点的距离(4d)
 		//适用于对齐SIMD优化方式
 		typedef std::vector<Vector4, STLAllocator<Vector4, CategorisedAlignAllocPolicy<MEMCATEGORY_GEOMETRY> > > TriangleFaceNormalList;
 
@@ -84,14 +85,14 @@ namespace Sapphire
 		/**
 		主三角形数组，保存所有的三角形的边表，注意：这些三角形针对边组进行分组
 		*/
-		//TriangleList triangles;
+		TriangleList triangles;
 		/**
 		所有的三角形面法线，它与三角形和一一对应*/
-		//TriangleFaceNormalList triangleFaceNormals;
+		TriangleFaceNormalList triangleFaceNormals;
 		/** 三角形朝向状态。它与三角形和一一对应 */
-		//TriangleLightFacingList triangleLightFacings;
+		TriangleLightFacingList triangleLightFacings;
 		/** 这边边表所有的边组*/
-		//EdgeGroupList edgeGroups;
+		EdgeGroupList edgeGroups;
 		/** 指示网格是多方面的标准 */
 		bool isClosed;
 
@@ -99,27 +100,135 @@ namespace Sapphire
 		/**
 		在这个边表里计算三角形光源朝向状态
 		@remarks
-		这是通常是计算轮廓的第一步，
-		This is normally the first stage of calculating a silhouette, i.e.
-		establishing which tris are facing the light and which are facing
-		away. This state is stored in the 'triangleLightFacings'.
-		@param lightPos 4D position of the light in object space, note that
-		for directional lights (which have no position), the w component
-		is 0 and the x/y/z position are the direction.
+		这是通常是计算轮廓的第一步，建立这个三角形是面向光源或是背离光源。
+		这个状态在'triangleLightFacings'
+		@param lightPos 是光源在对象空间中的4D位置， 注意：对于之平行光（没有位置的），其w分量是0和xyz位置是其方向
 		*/
 		void updateTriangleLightFacing(const Vector4& lightPos);
-		/** Updates the face normals for this edge list based on (changed)
-		position information, useful for animated objects.
-		@param vertexSet The vertex set we are updating
-		@param positionBuffer The updated position buffer, must contain ONLY xyz
+
+		/**
+		更新这个边表基于位置信息的面法线，用于动画对象
+		@param vertexSet 我们更新的顶点集合
+		@param positionBuffer 更新位置缓冲区，必须只包含XYZ
 		*/
 		//void updateFaceNormals(size_t vertexSet, const HardwareVertexBufferSharedPtr& positionBuffer);
 
 
 
-		// Debugging method
+		// DEBUG的方法
 		void log(Log* log);
 
+	};
+
+	/** General utility class for building edge lists for geometry.
+	@remarks
+	You can add multiple sets of vertex and index data to build and edge list.
+	Edges will be built between the various sets as well as within sets; this allows
+	you to use a model which is built from multiple SubMeshes each using
+	separate index and (optionally) vertex data and still get the same connectivity
+	information. It's important to note that the indexes for the edge will be constrained
+	to a single vertex buffer though (this is required in order to render the edge).
+	*/
+	class _SapphireExport EdgeListBuilder
+	{
+	public:
+
+		EdgeListBuilder();
+		virtual ~EdgeListBuilder();
+		/** Add a set of vertex geometry data to the edge builder.
+		@remarks
+		You must add at least one set of vertex data to the builder before invoking the
+		build method.
+		*/
+		void addVertexData(const VertexData* vertexData);
+		/** Add a set of index geometry data to the edge builder.
+		@remarks
+		You must add at least one set of index data to the builder before invoking the
+		build method.
+		@param indexData The index information which describes the triangles.
+		@param vertexSet The vertex data set this index data refers to; you only need to alter this
+		if you have added multiple sets of vertices
+		@param opType The operation type used to render these indexes. Only triangle types
+		are supported (no point or line types)
+		*/
+		void addIndexData(const IndexData* indexData, size_t vertexSet = 0,
+			RenderOperation::OperationType opType = RenderOperation::OT_TRIANGLE_LIST);
+
+		/** Builds the edge information based on the information built up so far.
+		@remarks
+		The caller takes responsibility for deleting the returned structure.
+		*/
+		EdgeData* build(void);
+
+		/// Debugging method
+		void log(Log* l);
+	protected:
+
+		/** A vertex can actually represent several vertices in the final model, because
+		vertices along texture seams etc will have been duplicated. In order to properly
+		evaluate the surface properties, a single common vertex is used for these duplicates,
+		and the faces hold the detail of the duplicated vertices.
+		*/
+		struct CommonVertex {
+			Vector3  position;  // location of point in euclidean space
+			size_t index;       // place of vertex in common vertex list
+			size_t vertexSet;   // The vertex set this came from
+			size_t indexSet;    // The index set this was referenced (first) from
+			size_t originalIndex; // place of vertex in original vertex set
+		};
+		/** A set of indexed geometry data */
+		struct Geometry {
+			size_t vertexSet;           // The vertex data set this geometry data refers to
+			size_t indexSet;            // The index data set this geometry data refers to
+			const IndexData* indexData; // The index information which describes the triangles.
+			RenderOperation::OperationType opType;  // The operation type used to render this geometry
+		};
+		/** Comparator for sorting geometries by vertex set */
+		struct geometryLess {
+			bool operator()(const Geometry& a, const Geometry& b) const
+			{
+				if (a.vertexSet < b.vertexSet) return true;
+				if (a.vertexSet > b.vertexSet) return false;
+				return a.indexSet < b.indexSet;
+			}
+		};
+		/** Comparator for unique vertex list */
+		struct vectorLess {
+			bool operator()(const Vector3& a, const Vector3& b) const
+			{
+				if (a.x < b.x) return true;
+				if (a.x > b.x) return false;
+				if (a.y < b.y) return true;
+				if (a.y > b.y) return false;
+				return a.z < b.z;
+			}
+		};
+
+		typedef vector<const VertexData*>::type VertexDataList;
+		typedef vector<Geometry>::type GeometryList;
+		typedef vector<CommonVertex>::type CommonVertexList;
+
+		GeometryList mGeometryList;
+		VertexDataList mVertexDataList;
+		CommonVertexList mVertices;
+		EdgeData* mEdgeData;
+		/// Map for identifying common vertices
+		typedef map<Vector3, size_t, vectorLess>::type CommonVertexMap;
+		CommonVertexMap mCommonVertexMap;
+		/** Edge map, used to connect edges. Note we allow many triangles on an edge,
+		after connected an existing edge, we will remove it and never used again.
+		*/
+		typedef multimap< std::pair<size_t, size_t>, std::pair<size_t, size_t> >::type EdgeMap;
+		EdgeMap mEdgeMap;
+
+		void buildTrianglesEdges(const Geometry &geometry);
+
+		/// Finds an existing common vertex, or inserts a new one
+		size_t findOrCreateCommonVertex(const Vector3& vec, size_t vertexSet,
+			size_t indexSet, size_t originalIndex);
+		/// Connect existing edge or create a new edge - utility method during building
+		void connectOrCreateEdge(size_t vertexSet, size_t triangleIndex, size_t vertIndex0, size_t vertIndex1,
+			size_t sharedVertIndex0, size_t sharedVertIndex1);
 	};
 }
 
